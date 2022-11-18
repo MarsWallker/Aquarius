bd087de83e0c305777cb320c1f2f286de82410c2
MarsWallker <94367783+MarsWallker@users.noreply.github.com>
2022-11-17 17:57:43 -0800

    Update Motor Code without pot values.ino

--- Motor Code without pot values.ino
+++ Motor Code without pot values.ino
@@ -1,18 +1,15 @@
-/*
-        Arduino Brushless Motor Control
-     by Dejan, https://howtomechatronics.com
-*/
-
 #include <Servo.h>
 
 Servo ESC;     // create servo object to control the ESC
+Servo m2;
 
 int potValue;  // value from the analog pin
 
 void setup() {
   // Attach the ESC on pin 9
+   m2.attach(10,1000,2000);
   ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
-  ESC.attach(10,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+// (pin, min pulse width, max pulse width in microseconds) 
 
   // Attempt to arm the speed controller
   ESC.write(0);
@@ -24,8 +21,24 @@ void setup() {
   ESC.write(0);
   delay(500);
   
+  
+
+  // (pin, min pulse width, max pulse width in microseconds) 
+// (pin, min pulse width, max pulse width in microseconds) 
+
+  // Attempt to arm the speed controller
+  m2.write(0);
+  delay(500);
+
+  m2.write(90);
+  delay(500);
+
+   m2.write(0);
+  delay(500);
+  
   pinMode(9,OUTPUT);
-  pinMode(10,OUTPUT); 
+  pinMode(10,OUTPUT);
+   
 }
 
 void loop() {
@@ -33,4 +46,5 @@ void loop() {
   potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
   // ESC.write(potValue);    // Send the signal to the ESC
   ESC.write(120);
+  m2.write(120);
 }
