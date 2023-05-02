e2146a02a6aff9d18c5bdd2247431ee3755324ba
MarsWallker <94367783+MarsWallker@users.noreply.github.com>
2023-05-02 15:44:57 -0700

    Update ToF_sensor_code_MedMode.ino

--- ToF_sensor_code_MedMode.ino
+++ ToF_sensor_code_MedMode.ino
@@ -1,48 +1,45 @@
 /*
-  Reading distance from the laser based VL53L1X
-  By: kaloha
-  CQRobot
-  VL53L1X Distance Sensor from CQRobot: http://www.cqrobot.wiki/index.php/VL53L1X_Distance_Sensor
-  This example demonstrates how to read the distance in medium mode(up to 4m) and the measurement status.
+This example shows how to take simple range measurements with the VL53L1X. The
+range readings are in units of mm.
 */
 
 #include <Wire.h>
-#include "VL53L1X.h"
+#include <VL53L1X.h>
 
-VL53L1X Distance_Sensor;
+VL53L1X sensor;
 
 void setup()
 {
+  while (!Serial) {}
+  Serial.begin(9600);
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
-  Serial.begin(115200);
-  Serial.println("VL53L1X Distance Sensor tests in medium distance mode(up to 3m).");
-  Distance_Sensor.setTimeout(500);
-  if (!Distance_Sensor.init())
+  sensor.setTimeout(500);
+  if (!sensor.init())
   {
-    Serial.println("Failed to initialize VL53L1X Distance_Sensor!");
+    Serial.println("Failed to detect and initialize sensor!");
     while (1);
   }
-  
+
   // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
   // You can change these settings to adjust the performance of the sensor, but
-  // the minimum timing budget is 33 ms for medium and long distance mode
-  Distance_Sensor.setDistanceMode(VL53L1X::Medium);
-  Distance_Sensor.setMeasurementTimingBudget(50000);
+  // the minimum timing budget is 20 ms for short distance mode and 33 ms for
+  // medium and long distance modes. See the VL53L1X datasheet for more
+  // information on range and timing limits.
+  sensor.setDistanceMode(VL53L1X::Long);
+  sensor.setMeasurementTimingBudget(50000);
 
   // Start continuous readings at a rate of one measurement every 50 ms (the
   // inter-measurement period). This period should be at least as long as the
   // timing budget.
-  Distance_Sensor.startContinuous(50);
+  sensor.startContinuous(50);
 }
 
 void loop()
 {
-  Distance_Sensor.read();
-  Serial.print("Distance(mm): ");
-  Serial.print(Distance_Sensor.ranging_data.range_mm);
-   Serial.print("\tStatus: ");
-  Serial.print(VL53L1X::rangeStatusToString(Distance_Sensor.ranging_data.range_status));
+  Serial.print(sensor.read());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
   Serial.println();
 }
