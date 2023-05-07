f85c2bf6a477c32fc0d72db4ee2d16f7a62df24b
MarsWallker <94367783+MarsWallker@users.noreply.github.com>
2023-05-06 18:39:31 -0700

    Update thermistor.ino

--- thermistor.ino
+++ thermistor.ino
@@ -1,5 +1,5 @@
 int ThermistorPin = 0;
-int Vo, Tc, Tf;
+int Vo;
 float R1 = 10000;
 float logR2, R2, T;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
@@ -14,14 +14,12 @@ void loop() {
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
-  Tc = T - 273.15;
-  Tf = (Tc * 9.0)/ 5.0 + 32.0; 
+  T = T - 273.15;
+  T = (T * 9.0)/ 5.0 + 32.0; 
 
   Serial.print("Temperature: "); 
-  Serial.print(Tf);
-  Serial.print(" F; ");
-  Serial.print(Tc);
-  Serial.println(" C");   
+  Serial.print(T);
+  Serial.println(" F"); 
 
   delay(500);
 }
